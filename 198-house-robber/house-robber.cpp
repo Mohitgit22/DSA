25867d275fa86af4a0bbb806a879fcb974b091ef
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-28 00:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.74%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -34,23 +34,43 @@ public:
     // }
 
     //bottom_up
-    int bottom_up(vector<int>& nums, int n, vector<int>& dp){
+    // int bottom_up(vector<int>& nums, int n, vector<int>& dp){
         
-        dp[0] = 0;
-        dp[1] = nums[0];
+    //     dp[0] = 0;
+    //     dp[1] = nums[0];
+
+    //     for(int i = 2; i <= n; i++){
+    //         int steal = nums[i-1] + dp[i-2];
+    //         int skip = dp[i-1];
+
+    //         dp[i] = max(steal, skip);
+    //     }
+    //     return dp[n];
+    // }
+
+
+    //space_optimized_bottom_up
+    int bottom_up(vector<int>& nums, int n, vector<int>& dp){
+        int prev2 = 0;
+        int prev1 = nums[0];
+        int ans = -1;
 
         for(int i = 2; i <= n; i++){
-            int steal = nums[i-1] + dp[i-2];
-            int skip = dp[i-1];
-
-            dp[i] = max(steal, skip);
+            int steal = nums[i-1] + prev2;
+            int skip = prev1;
+            // dp[i] = max(steal, skip);
+            ans = max(steal, skip);
+            
+            prev2 = prev1;
+            prev1 = ans;
         }
-        return dp[n];
+        return ans;
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-
+         if(n<=1)
+         return nums[0];
         // return maxrob(nums, n-1);
 
         // vector<int> dp(n,-1);
