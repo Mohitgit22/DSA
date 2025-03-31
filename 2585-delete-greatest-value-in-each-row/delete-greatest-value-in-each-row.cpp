040ec59b502ad666dad2bbe50f79d1b0d7546cf3
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-31 12:18:59 +0530

    Time: 2 ms (49.21%) | Memory: 15.2 MB (7.47%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.cpp
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
-        
-        int m = grid.size();
-        int n = grid[0].size();
+         int m = grid.size();
+         int n = grid[0].size();
 
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        priority_queue<pair<int, int>> each_row;
-        priority_queue<int> across_rows;
-        
-        int cnt = n;
-        int ans = 0;
-        
-       while(cnt>0){
-        for(int i = 0; i < m ; i++) {
-            for(int j = 0 ; j < n; j++) {
-                if(visited[i][j] == false){
-                each_row.push({grid[i][j], j});
-                }
+        vector<vector<int>> sortedgrid;
+        vector<int> arr;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0 ; j< n; j++) {
+                arr.push_back(grid[i][j]);
             }
-            across_rows.push(each_row.top().first);
-            int jth_index = each_row.top().second;
-            visited[i][jth_index] = true;
-            each_row = priority_queue<pair<int, int>>(); 
+            sort(arr.begin(), arr.end());
+            sortedgrid.push_back(arr);
+            arr.clear();
         }
-        ans += across_rows.top();
-        across_rows = priority_queue<int>();
-        cnt--;
-    }
-    return ans;
-    }
 
+    //  for(auto x : sortedgrid){
+    //     for( auto y : x){
+    //         cout<<y<<" ";
+    //     }
+    //     cout<<endl;
+    //  }
+        priority_queue<int> maxh;
+        int ans = 0;
+        while(n > 0){
+        for(int i = 0; i <m; i++) {
+                maxh.push(sortedgrid[i][n-1]);
+        }
+        ans += maxh.top();
+        maxh = priority_queue<int>();
+        n--;
+        }
+        return ans;
+    }
 };
