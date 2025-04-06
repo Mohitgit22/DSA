8b3a88933df6525c5690b8dd0d1895cba6357c98
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-04-06 18:00:25 +0530

    Time: 3 ms (19.09%) | Memory: 23.1 MB (23.77%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,20 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    pair<int, TreeNode*> helper(TreeNode* node) {
-        if (!node) return {0, nullptr};
+    TreeNode* lca(TreeNode* root,unordered_map<int,int>& mp ,int maxd){
+        if(!root) return root;
 
-        auto left = helper(node->left);
-        auto right = helper(node->right);
+       if(mp[root->val] == maxd)
+       return root;
 
-        if (left.first == right.first)
-            return {left.first + 1, node};
-        else if (left.first > right.first)
-            return {left.first + 1, left.second};
-        else
-            return {right.first + 1, right.second};
+        TreeNode* left = lca(root->left, mp, maxd);
+        TreeNode* right = lca(root->right, mp, maxd);
+
+        if(left!=nullptr and right!=nullptr)
+        return root;
+
+        if(left) return left;
+        else 
+        return right;
     }
 
+    void depth(TreeNode* root, unordered_map<int,int>& mp , int currdepth, int& maxd){
+        if(!root ) return ;
+
+        mp[root->val] = currdepth;
+        maxd = max(maxd, currdepth);
+        depth(root->left, mp, currdepth+1, maxd);
+        depth(root->right, mp, currdepth+1, maxd);
+    }
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return helper(root).second;
+        unordered_map<int,int> mp;
+        int maxd = 0;
+        depth(root, mp, 0, maxd);
+        cout<<maxd;
+        return lca(root, mp, maxd);
     }
 };
