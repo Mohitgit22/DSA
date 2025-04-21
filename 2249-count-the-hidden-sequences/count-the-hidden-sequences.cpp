e3eec2fb4b8745511418d3c21130ff9d81dd3160
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-04-21 13:50:19 +0530

    Time: 115 ms (5.64%) | Memory: 112.7 MB (47.58%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    bool check(vector<int>& differences,int idx, int lower, int upper, vector<int>& customvec){
+    int maxi = INT_MIN;
+    int mini = INT_MAX;
+
+    bool check(vector<int>& differences,int idx, int lower, int upper){
        int n = differences.size();
        int i = 0;
        
-       customvec.push_back(idx);
+       maxi = idx, mini = idx;
+    //    customvec.push_back(idx);
 
        while(i < n){
             idx += differences[i];
-            customvec.push_back(idx);
+            // customvec.push_back(idx);
+            maxi = max(maxi, idx);
+            mini = min(mini, idx);
             if(idx < lower || idx > upper) {
-                customvec.clear();
+                // customvec.clear();
                 return false;
             }
         i++;
@@ -19,15 +25,18 @@ public:
     }
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
         
-        vector<int> customvec;
+        // vector<int> customvec;
+        bool flag = false;
         for(int i = lower;i <= upper; i++){
-            if(check(differences, i, lower, upper, customvec))
+            if(check(differences, i, lower, upper)){
+                flag = true;
             break;
+            }
         }
-        if(customvec.size() == 0) return 0;
-
-        int maxi = *max_element(customvec.begin(), customvec.end());
-        int mini = *min_element(customvec.begin(), customvec.end());
+        if(!flag) return 0;
+        
+        // int maxi = *max_element(customvec.begin(), customvec.end());
+        // int mini = *min_element(customvec.begin(), customvec.end());
         int range = abs(upper-maxi) + abs(lower-mini)+1;
         return range;
     }
