90c4383619d601ddb0cf034da67834ad2e5181f8
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-10-27 16:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.21%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool search(vector<int>& row, int t){
+        for(int it: row){
+            if(it == t) return true;
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+       
+  
+
+        vector<int> row;
+        for(int i = 0; i < m; i++){
+            row.push_back(matrix[i][0]);
+        }
+
+        auto it = upper_bound(row.begin(), row.end(), target);
+        int ind = it - row.begin() - 1;
+       
+        if(ind < 0) return false;
+
+        // cout<<ind<<" indes us";
+        row = matrix[ind];
+        return search(row, target);
+    }
+};
