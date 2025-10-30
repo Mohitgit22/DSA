fb86a91e96295fa1f6eb689c6ce18d9c3a7f953d
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-10-30 11:24:06 +0530

    Time: 126 ms (60.72%) | Memory: 111.1 MB (23.40%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int t, int cur_sum, int ind,vector<vector<int>> & dp){
-        if(cur_sum == t) return true;
+    // bool solve(vector<int>& nums, int t, int cur_sum, int ind,vector<vector<int>> & dp){
+    //     if(cur_sum == t) return true;
 
-        if(ind <= 0) return false;
+    //     if(ind <= 0) return false;
 
-        if(dp[ind][cur_sum]!= -1) return dp[ind][cur_sum];
+    //     if(dp[ind][cur_sum]!= -1) return dp[ind][cur_sum];
 
-        bool notpick = solve(nums, t, cur_sum, ind-1, dp);
+    //     bool notpick = solve(nums, t, cur_sum, ind-1, dp);
 
-        bool pick = false;
-        if(cur_sum + nums[ind-1] <= t){
-            pick = solve(nums, t, cur_sum + nums[ind-1], ind-1, dp);
-        }
+    //     bool pick = false;
+    //     if(cur_sum + nums[ind-1] <= t){
+    //         pick = solve(nums, t, cur_sum + nums[ind-1], ind-1, dp);
+    //     }
 
-        return dp[ind][cur_sum] = pick || notpick;
-    }
+    //     return dp[ind][cur_sum] = pick || notpick;
+    // }
 
 
     bool canPartition(vector<int>& nums) {
@@ -24,8 +24,29 @@ public:
         if(sum %2 != 0)
         return false;
         
+        sum = sum/2;
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
-        return solve(nums, sum/2, 0, n, dp);
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, false));
+
+        for(int i = 0;i < n+1; i++){
+            for(int j= 0; j < sum+1; j++){
+               if(i == 0)
+               dp[i][j]= false;
+
+               if(j == 0)
+               dp[i][j] = true;
+            }
+        }
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < sum+1; j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
     }
 };
