66793432d85a3778f6d8735a057406c5d9047ddd
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-10-30 11:15:42 +0530

    Time: 164 ms (26.51%) | Memory: 205.6 MB (7.17%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int k;
-    int n;
-    
-    bool solve(int i, int sum, vector<int>& nums) {
-        if (i >= n)
-            return (sum == k / 2);
-        
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
-        
-        bool include = false;
-        if (sum + nums[i] <= k / 2) {
-            include = solve(i + 1, sum + nums[i], nums);
+    bool solve(vector<int>& nums, int t, int cur_sum, int ind,vector<vector<int>> & dp){
+        if(cur_sum == t) return true;
+
+        if(ind <= 0) return false;
+
+        if(dp[ind][cur_sum]!= -1) return dp[ind][cur_sum];
+
+        bool notpick = solve(nums, t, cur_sum, ind-1, dp);
+
+        bool pick = false;
+        if(cur_sum + nums[ind-1] <= t){
+            pick = solve(nums, t, cur_sum + nums[ind-1], ind-1, dp);
         }
-        bool exclude = solve(i + 1, sum, nums);
-        
-        return dp[i][sum] = include || exclude;
+
+        return dp[ind][cur_sum] = pick || notpick;
     }
-    
+
+
     bool canPartition(vector<int>& nums) {
-        n = nums.size();
-        k = 0;
-        for (auto x : nums) {
-            k += x;
-        }
-        if (k % 2 != 0) {
-            return false;
-        }
-        dp.assign(n, vector<int>(k / 2 + 1, -1));
-        return solve(0, 0, nums);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if(sum %2 != 0)
+        return false;
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        return solve(nums, sum/2, 0, n, dp);
     }
 };
