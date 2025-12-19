1331a23f80236405d6c0b46359d601150119aaae
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-12-19 13:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (43.14%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -3,37 +3,33 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n = image[0].size();
-
-        if (image[sr][sc] == color)
+      if (image[sr][sc] == color)
        return image;
+        int curr = image[sr][sc];
 
+        image[sr][sc] = color;
 
         queue<pair<int, int>> que;
         que.push({sr, sc});
-
-        int c = image[sr][sc];
-
-        image[sr][sc]=color;
-
-        int dx[4] = {0, 0, -1,1 };
+    
+        int dx[4] = {0, 0, -1, 1};
         int dy[4] = {-1, 1, 0, 0};
         while(!que.empty()){
-            auto front = que.front();
+            int x = que.front().first;
+            int y = que.front().second;
+
             que.pop();
-            int x = front.first;
-            int y = front.second;
-            
-            for(int i = 0; i < 4; i++){
+            for(int i = 0 ;i < 4;i++){
                 int newx = x + dx[i];
                 int newy = y + dy[i];
 
-                if(newx >= 0 and newy >= 0 and newx < m and newy < n and image[newx][newy] == c){
+                if(newx >= 0 and newy >= 0 and newx < m and newy < n and image[newx][newy] ==  curr){
                     image[newx][newy] = color;
                     que.push({newx, newy});
                 }
             }
         }
 
-        return image;
+       return image;
     }
 };
