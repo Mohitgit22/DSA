a9f2e928a92cbbaa51482a1e9ccdc97eb3051765
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-12-19 12:33:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (73.91%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        
         queue<pair<int, pair<int, int>>> que;
 
         int m = grid.size();
         int n = grid[0].size();
 
         for(int i = 0; i < m; i++){
-            for(int j = 0;j < n; j++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == 2){
                     que.push({0, {i, j}});
                 }
             }
         }
+       
+        int dx[4] = {0,0, -1, 1};
+        int dy[4] = {1, -1, 0, 0};
 
-      
-        int dx[4] = {0, 0 ,-1, 1};
-        int dy[4] = {-1, 1, 0, 0};
-        int maxi = INT_MIN;
-
+        int maxt = 0;
         while(!que.empty()){
-            auto front= que.front();
-            int t = front.first;
-            int x =front.second.first;
-            int y = front.second.second;
+            int t = que.front().first;
+            int x = que.front().second.first;
+            int y = que.front().second.second;
+            
+            maxt = max(maxt, t);
             que.pop();
-
-           maxi = max(maxi, t);
 
             for(int i = 0; i < 4; i++){
                 int newx = x + dx[i];
                 int newy = y + dy[i];
 
                 if(newx >= 0 and newy >= 0 and newx < m and newy < n and grid[newx][newy] == 1){
-                    grid[newx][newy] = 2;
-                    que.push({t+1,{newx, newy}});
+                     grid[newx][newy] = 2;
+                     que.push({t+1, {newx, newy}});
                 }
-            }
+            } 
         }
 
-         for(int i = 0; i < m; i++){
-            for(int j = 0;j < n; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1) return -1;
             }
         }
        
-        return maxi == INT_MIN ? 0 : maxi;
-
+       return maxt;
     }
 };
